9ca151d90bef463d9e83e3f7f5be79862697c19e
Tyapkov Mikhail <v_tyapkov@mail.ru>
2023-02-25 00:17:35 +0300

    nav+head

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import React from 'react';
+
+import { AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu } from './container';
+import { Navbar } from './components';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div>
+    <header className='header'>
+      <Navbar />
+      <Header />
+    </header>
+    <main className='main'>
+      <section>
+        <AboutUs />
+      </section>
+      <section>
+        <SpecialMenu />
+      </section>
+      <section>
+        <Chef />
+      </section>
+      <section>
+        <Intro />
+      </section>
+      <section>
+        <Laurels />
+      </section>
+      <section>
+        <Gallery />
+      </section>
+      <section>
+        <FindUs />
+      </section>
+    </main>
+    <footer className='footer'>
+      <Footer />
+    </footer>
+  </div>
+);
 
 export default App;
